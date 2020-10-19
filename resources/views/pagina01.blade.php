595d4cd4a6400d7644a132cb6538926a29f71bb0
drosso88 <rocio.soriano.gomez@gmail.com>
2020-10-19 18:44:40 +0200

    intentando hacer una plantilla para frontend pervirtiendo laravel

---
+++ resources/views/pagina01.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.ejemplolayout')
+
+@section('titulo', 'Página 01 del proyecto')
+
+@section ('barralateral')
+<p>Esta parte esta en la barra lateral</p>
+
+@endsection
+
+@section('contenido')
+
+<p>Estoy en to el body</p>
